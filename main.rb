6b5715e6a844b227e4038f0de2fca0da679ecb78
Leael95 <leaxplosion@gmail.com>
2022-08-09 19:06:36 -0300

    - Variables local to global
    - Only in one position

--- main.rb
+++ main.rb
@@ -1,11 +1,10 @@
 def game(width,height,*args)
   gamedimensions = Array.new(height) { Array.new(width) {'.'} }
-  nextgen = Array.new(height) { Array.new(width) {'.'} }
+  $nextgen = Array.new(height) { Array.new(width) {'.'} }
 
   def liveordeadcell(fila,columna,actualgame)
     cell = actualgame[fila][columna]
     countCells = 0
-  
     if cell == '*'
 
       if actualgame[fila - 1][columna - 1] == '*'
@@ -39,23 +38,53 @@ def game(width,height,*args)
       if actualgame[fila + 1][columna + 1] == '*'
         countCells += 1
       end
-      
-      puts countCells
+
+      if countCells != 2 && countCells != 3
+        $nextgen[fila][columna] = '.'
+      else
+        $nextgen[fila][columna] = '*'
+      end
+
+    elsif cell == '.'
+      if actualgame[fila - 1][columna - 1] == '*'
+        countCells += 1
+      end
+
+      if actualgame[fila - 1][columna] == '*'
+        countCells += 1
+      end
+
+      if actualgame[fila - 1][columna + 1] == '*'
+        countCells += 1
+      end
+
+      if actualgame[fila][columna - 1] == '*'
+        countCells += 1
+      end
+
+      if actualgame[fila][columna + 1] == '*'
+        countCells += 1
+      end
+
+      if actualgame[fila + 1][columna - 1] == '*'
+        countCells += 1
+      end
+
+      if actualgame[fila + 1][columna] == '*'
+        countCells += 1
+      end
+
+      if actualgame[fila + 1][columna + 1] == '*'
+        countCells += 1
+      end
+
+      if countCells == 3
+        $nextgen[fila][columna] = '*'
+      else
+        $nextgen[fila][columna] = '.'
+      end
     end
   end
-
-  # for b in 0..height-1 do
-
-  #   for c in 0..width-1 do
-  #     number = rand(1..5)
-
-  #     if number == 1
-  #       gamedimensions[b][c] = '*'
-  #     end
-
-  #   end
-
-  # end
 
   for position in 0..args[0].length-1 do
     positionX = args[0][position][0]
@@ -68,8 +97,28 @@ def game(width,height,*args)
     puts gamedimensions[a].join(' ')
   end
 
-  liveordeadcell 0,2,gamedimensions
+  puts '----------------------------'
+
+  for a in 0..height-1 do
+    puts $nextgen[a].join(' ')
+  end
 
 end
 
-game 5,8,[[0,2],[1,2],[2,1],[2,2],[2,3],[3,2],[4,2]]
+game 6,6,[[0,2],[1,2],[2,1],[2,2],[2,3],[3,1],[3,2],[3,3],[4,2]]
+
+
+
+  # Randomizador de posiciones de celulas vivas
+  # for b in 0..height-1 do
+
+  #   for c in 0..width-1 do
+  #     number = rand(1..5)
+
+  #     if number == 1
+  #       gamedimensions[b][c] = '*'
+  #     end
+
+  #   end
+
+  # end
